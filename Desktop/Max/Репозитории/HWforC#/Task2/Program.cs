fcd059c661072635053e63cf13e049ac9c0815de
Maksim Nosov <gohard174@mail.ru>
2023-02-21 19:31:12 +0300

    дз по семинару 2

---
+++ Desktop/Max/Репозитории/HWforC#/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая
+// принимает на вход три числа и выдаёт
+// максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+
+int max = numberA;
+if ( numberB > numberA )
+max = numberB;
+if (numberC > max)
+max = numberC;
+
+    Console.WriteLine($"{max} наибольшее число");
+
+
+
---
+++ Desktop/Max/Репозитории/seminar2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число");
+string? answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+int res = number * number;
+Console.WriteLine(res);
+
+if (res > 100);
+{
+    Console.WriteLine("Да");
+}
+
+
+
+
